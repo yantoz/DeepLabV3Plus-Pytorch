bf6f4d447e5d8b844d08a3646ad341257fdee0b3
joemarshall <joe.marshall@nottingham.ac.uk>
2020-06-08 16:13:45 +0100

    fix hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -12,41 +12,41 @@ from network import deeplabv3plus_mobilenet as _deeplabv3plus_mobilenet
 def deeplabv3plus_resnet50(pretrained=False,**kwargs):
   model=_deeplabv3plus_resnet50(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_resnet50_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_resnet50_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
   
 def deeplabv3_resnet50(pretrained=False,**kwargs):
   model=_deeplabv3_resnet50(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_resnet50_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_resnet50_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
 
 def deeplabv3_resnet101(pretrained=False,**kwargs):
   model=_deeplabv3_resnet101(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_resnet101_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_resnet101_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
 
 def deeplabv3plus_resnet101(pretrained=False,**kwargs):
   model=_deeplabv3plus_resnet101(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_resnet101_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_resnet101_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
 
 def deeplabv3_mobilenet(pretrained=False,**kwargs):
   model=_deeplabv3_mobilenet(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_mobilenet_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3_mobilenet_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
 
 def deeplabv3plus_mobilenet(pretrained=False,**kwargs):
   model=_deeplabv3plus_mobilenet(**kwargs)
   if pretrained:
-	checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_mobilenet_voc_os16.pth'
+    checkpoint = 'https://github.com/joemarshall/DeepLabV3Plus-Pytorch/releases/download/pretrained_1.0/best_deeplabv3plus_mobilenet_voc_os16.pth'
     model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
   return model
